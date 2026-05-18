95f614b1ed410cc63ac3c23fd77505910d83a483
Akshay Gupta <kitallis@gmail.com>
2026-05-18 12:26:16 +0530

    Add total-due to CC statement from CSV header

--- src/hdfc/credit_card_statement.clj
+++ src/hdfc/credit_card_statement.clj
@@ -22,10 +22,8 @@
      :credit?     credit?
      :tag         (description->tag desc)}))
 
-(defn parse-csv [filename]
-  (let [lines      (-> filename slurp s/split-lines)
-        header-idx (first (keep-indexed #(when (s/starts-with? %2 "Transaction type~") %1) lines))
-        new-fmt?   (s/starts-with? (nth lines header-idx) "Transaction type~|~")
+(defn parse-csv [lines new-fmt?]
+  (let [header-idx (first (keep-indexed #(when (s/starts-with? %2 "Transaction type~") %1) lines))
         delimiter  (if new-fmt? #"~\|~" #"~")]
     (->> (drop (inc header-idx) lines)
          (take-while #(or (s/starts-with? % "Domestic")
@@ -36,27 +34,37 @@
                   [tx-type name date desc a b c]
                   [tx-type name date desc b c a]))))))
 
-(defn gen-statement [credits debits]
+(defn parse-total-due [lines new-fmt?]
+  (let [delimiter (if new-fmt? #"~\|~" #"~")
+        line      (->> lines (filter #(s/starts-with? % "Total Amount Due~")) first)]
+    (-> line (s/split delimiter) second money/parse-amount)))
+
+(defn gen-statement [credits debits total-due]
   (let [[from to] (min-max-dates (concat credits debits))]
     {:from            from
      :to              to
      :total-credits   (reduce + 0.0 (map :amount credits))
      :total-debits    (reduce + 0.0 (map :amount debits))
+     :total-due       total-due
      :debit-breakdown (->> (group-by :tag debits)
                            (map (fn [[tag expenses]]
                                   [tag (reduce + 0.0 (map :amount expenses))]))
                            (into {}))}))
 
 (defn process [filename]
-  (let [rows                        (parse-csv filename)
+  (let [lines                       (-> filename slurp s/split-lines)
+        header-idx                  (first (keep-indexed #(when (s/starts-with? %2 "Transaction type~") %1) lines))
+        new-fmt?                    (s/starts-with? (nth lines header-idx) "Transaction type~|~")
+        rows                        (parse-csv lines new-fmt?)
         tx-maps                     (map parse-row rows)
         {credits true debits false} (group-by :credit? tx-maps)]
-    (gen-statement (or credits []) (or debits []))))
+    (gen-statement (or credits []) (or debits []) (parse-total-due lines new-fmt?))))
 
 (defn format-statement [data]
   (let [summary   {:period        (str (:from data) " to " (:to data))
                    :total-credits (money/format-amount (:total-credits data))
-                   :total-debits  (money/format-amount (:total-debits data))}
+                   :total-debits  (money/format-amount (:total-debits data))
+                   :total-due     (money/format-amount (:total-due data))}
         breakdown (for [[category amount] (:debit-breakdown data)]
                     {:category (name category) :amount (money/format-amount amount)})]
     (println)
